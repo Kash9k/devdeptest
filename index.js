88bb865b4c7c9d263641db19409ef091596f56f6
PrakashKarri <kash9k@gmail.com>
2018-07-23 22:57:04 -0400

    added

---
+++ index.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var mocha = require('mocha');
+
+console.log("chai... START");
+console.log(chai);
+console.log("Printing... mocha");
+console.log(mocha);
+console.log("chai... END");
